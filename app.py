5b4f16a2a117bc3a8f7c466d183e3b6aceca651b
Vishal Gowda V N <vishalgowdaavn27@gmail.com>
2025-08-11 19:48:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
-import streamlit as st
+import gradio as gr
 from chatbot import load_data, get_answer
-
-st.title("🤖 E&C Concepts Chatbot")
-st.markdown("Ask questions from your E&C subjects! from Vishal Gowda ")
 
 data = load_data()
 
-sem = st.selectbox("Select Semester", list(data.keys()))
-subject = st.selectbox("Select Subject", list(data[sem].keys()))
-question = st.text_input("Enter your question")
+def chatbot_interface(sem, subject, question):
+    return get_answer(data, sem, subject, question)
 
-if st.button("Ask"):
-    answer = get_answer(data, sem, subject, question)
-    st.success(answer) 
+semesters = list(data.keys())
+subjects_per_sem = {sem: list(data[sem].keys()) for sem in semesters}
 
+def update_subjects(sem):
+    return gr.Dropdown.update(choices=subjects_per_sem[sem])
+
+with gr.Blocks() as demo:
+    gr.Markdown("## 🤖 E&C Concepts Chatbot\nAsk questions from your E&C subjects! from Vishal Gowda")
+    
+    sem_dropdown = gr.Dropdown(choices=semesters, label="Select Semester")
+    subject_dropdown = gr.Dropdown(choices=subjects_per_sem[semesters[0]], label="Select Subject")
+    question_box = gr.Textbox(label="Enter your question")
+    answer_box = gr.Textbox(label="Answer", interactive=False)
+    
+    sem_dropdown.change(fn=update_subjects, inputs=sem_dropdown, outputs=subject_dropdown)
+    submit_btn = gr.Button("Ask")
+    submit_btn.click(fn=chatbot_interface, inputs=[sem_dropdown, subject_dropdown, question_box], outputs=answer_box)
+
+demo.launch()
